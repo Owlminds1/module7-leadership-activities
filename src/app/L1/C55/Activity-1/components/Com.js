45d7c8f6e039cb0c3f57792ee60ab7b10d7dfbd6
mlalani <mlalani62@gmail.com>
2025-09-30 11:43:01 +0530

    bug fix l1 c55 a1

--- src/app/L1/C55/Activity-1/components/Com.js
+++ src/app/L1/C55/Activity-1/components/Com.js
@@ -2,11 +2,11 @@
 
 import { useState } from 'react'
 import Image from 'next/image'
-import S1 from "../assets/S1.jpg"
-import S2 from "../assets/S2.jpg"
-import S3 from "../assets/S3.jpg"
-import S4 from "../assets/S4.jpg"
-import S5 from "../assets/S5.jpg"
+import S1 from "../assets/s1.jpg"
+import S2 from "../assets/s2.jpg"
+import S3 from "../assets/s3.jpg"
+import S4 from "../assets/s4.jpg"
+import S5 from "../assets/s5.jpg"
 
 export default function Com() {
   const [currentScreen, setCurrentScreen] = useState(0)
